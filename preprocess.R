c40aaaf2f028f6a6ef51bc1594d4c771c13afd76
Adrian <Adrian@Adrian-Komputer>
2012-01-23 03:21:45 +0100

    a

--- preprocess.R
+++ preprocess.R
@@ -6,6 +6,7 @@ library("Snowball") # stemmer
 library("e1071")	# bayes
 library("knnflex")	# knn
 library("kernlab")	# svm
+library("lsa")		# cosine distance
 
 # preprocessing options
 preprocess.options.stemming <- TRUE
@@ -213,10 +214,16 @@ transform.randomize<-function(data) {
 	return(result)
 }
 
-spamfilter.opt.knn.k <- 3
 spamfilter.opt.nb.threshold <- 0.001
-spamfilter.opt.k <- 3
-spamfilter.opt.folds <- 10
+
+spamfilter.opt.knn.k <- 3
+spamfilter.opt.knn.dist <-"euclidean" # "manhattan" # "cosine"
+
+spamfilter.opt.svm.type <- "C-svc"
+spamfilter.opt.svm.kernel <- "rbfdot"
+spamfilter.opt.svm.cost <- 1
+
+spamfilter.opt.folds <- 5
 
 spamfilter.crossValidate <- function(data, modelFunction, folds) {
 	testDataPerFold <- nrow(data) / folds
@@ -245,7 +252,7 @@ spamfilter.crossValidate <- function(data, modelFunction, folds) {
 	
 	allFp <- allFp / nrow(data)
 	allFn <- allFn / nrow(data)
-	acc <- 1 - (allFp+allFn)/nrow(data)
+	acc <- 1 - (allFp+allFn)
 	
 	return(list("fpr"=allFp, "fnr"=allFn, "acc"=acc))
 }
@@ -253,18 +260,32 @@ spamfilter.crossValidate <- function(data, modelFunction, folds) {
 spamfilter.bayes <- function(data, dataIndices, testIndices) {
 	model <- naiveBayes(spam ~ ., data, dataIndices)
 	results <- predict(model, data[testIndices,-1], threshold=spamfilter.opt.nb.threshold)
-	print("BAYES")
-	print(results)
 	return(results)
 }
 
 spamfilter.knn <- function(data, dataIndices, testIndices) {
-	results<-knn(data[dataIndices,-1], data[testIndices,-1],data[dataIndices,1], spamfilter.opt.knn.k)
-	return(results)
+	
+	dist <- matrix()
+	
+	print("comuputing distance matrix")
+	
+	if( spamfilter.opt.knn.dist == "cosine") {
+		dist <- cosine(t(as.matrix(d[,-1])))
+	}else{
+		dist <- knn.dist(as.matrix(data[,-1]), dist.meth=spamfilter.opt.knn.dist)
+	}
+	
+	print("predicting")
+	results <- knn.predict(dataIndices, testIndices, data[dataIndices,1], dist, k=spamfilter.opt.knn.k)
+	return(as.numeric(results))
 }
 
 spamfilter.svm <- function(data, dataIndices, testIndices) {
-	model <- ksvm(spam ~ ., data[dataIndices,])
+	model <- ksvm(spam ~ ., data[dataIndices,], 
+			type=spamfilter.opt.svm.type,
+			kernel=spamfilter.opt.svm.kernel,
+			C=spamfilter.opt.svm.cost)
+	
 	results <- predict(model, data[testIndices,-1])
 	return(results)
 }
@@ -273,7 +294,7 @@ spamfilter.test_bayes_threshold<-function(data) {
 	old <- spamfilter.opt.nb.threshold
 	result <- list()
 	
-	for(threshold in c(0.1) ) { #c(0.5, 0.3, 0.2, 0.1, 0.05, 0.01, 0.001, 0.0001, 0.00001, 0)
+	for(threshold in c(0.5, 0.1, 0.01, 0.001, 0) ) {
 		spamfilter.opt.nb.threshold <<- threshold
 		error <- spamfilter.crossValidate(data, spamfilter.bayes, spamfilter.opt.folds)
 		result[[as.character(threshold)]] <- error
@@ -287,26 +308,87 @@ spamfilter.test_bayes_threshold<-function(data) {
 spamfilter.test_bayes_apriori<-function(data) {
 	result <- list()
 	
-	isSpam <- data[,1] == 1
+	isSpam <- (data[,1] == 1)
 	spam <- data[isSpam,]
 	ham <- data[!isSpam,]
-	spamCount <- length(spam)
-	hamCount <- length(ham)
+	spamCount <- nrow(spam)
+	hamCount <- nrow(ham)
 	
-	if(2*spamCount > hamCount) {
-		print("WARNING: this test assumes that there is 2 times more spam messages available tham ham!")
+	if(spamCount > hamCount) {
+		print(c("WARNING: this test assumes that there is more spam messages available tham ham!", "spam", spamCount, "ham", hamCount))
+		return()
 	}
 	
-	for(ratio in c(2, 1.75, 1.5, 1, 0.5, 0.25)) {
-		input <- rbind(spam, ham[1:spamCount*ratio,])
+	for(ratio in c(5, 3, 2, 1.75, 1.5, 1, 0.5, 0.25)) {
+		
+		# here we lack more ham data, so we reduce spam data sent
+		if( ratio > 2.5) {
+			input <- rbind(spam[1:(2*spamCount/ratio), ] , ham[1:2*spamCount,])
+		} else {
+			input <- rbind(spam, ham[1:spamCount*ratio,])
+		}
+		
 		input <- transform.randomize(input)
 		error <- spamfilter.crossValidate(input, spamfilter.bayes, spamfilter.opt.folds)
 		result[[as.character(ratio)]] <- error
 	}
 	
+	
+	
 	return(result)
 }
 
+spamfilter.test_knn<-function(data) {
+	
+	results <- list();
+	
+	for(dist in c("euclidean", "manhattan", "cosine")) {
+		for(k in c(3,7,15)) {
+			
+			oldk <- spamfilter.opt.knn.k
+			oldd <- spamfilter.opt.knn.dist
+			
+			spamfilter.opt.knn.k <- k
+			spamfilter.opt.knn.dist <- dist
+			
+			error <- spamfilter.crossValidate(data, spamfilter.knn, spamfilter.opt.folds)
+			error$k <- k
+			error$dist <- dist
+			
+			results[[paste(dist, k)]] <- error
+			
+			spamfilter.opt.knn.k <- oldk
+			spamfilter.opt.knn.dist <- oldd
+		}
+	}
+	
+	return(results)
+}
+
+spamfilter.test_svm<-function(data) {
+	results <- list();
+	
+	for(kernel in c("rbfdot", "polydot", "vanilladot")) {
+		for(cost in c(0.1, 1, 10)) {
+			oldk <- spamfilter.opt.svm.kernel
+			oldc <- spamfilter.opt.svm.cost
+			
+			spamfilter.opt.svm.kernel <- k
+			spamfilter.opt.svm.cost <- dist
+			
+			error <- spamfilter.crossValidate(data, spamfilter.svm, spamfilter.opt.folds)
+			error$kernel <- kernel
+			error$cost <- cost
+			
+			results[[paste(kernel, cost)]] <- error
+			
+			spamfilter.opt.svm.kernel <- oldk
+			spamfilter.opt.svm.cost <- oldc
+		}
+	}
+	
+	return(results)
+}
 #problem <- preprocess.do()
 #problem$dataset <- transform.toBinary(problem$randomize)
 #problem$dataset_binary <- transform.toBinary(problem$dataset)
@@ -315,8 +397,8 @@ spamfilter.test_bayes_apriori<-function(data) {
 #write.table(problem$dataset_binary, "./data_bin.txt")
 #write.table(problem$dataset_tfidf, "./data_tf.txt")
 
-#dataset <- read.table("./data.txt")
-#dataset_binary <- read.table("./data_bin.txt")
-#dataset_tfidf <- read.table("./data_tf.txt")
-
+#d <- read.table("d2.txt")
+#d_bin <- transform.toBinary(d)
+#d$spam <- factor(d$spam)
+#d_bin$spam <- factor(d_bin$spam)
 
